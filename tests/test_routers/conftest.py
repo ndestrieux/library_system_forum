3dcc787b5565c774e0a384fa37bd8b60bbf427ce
ndestrieux <n.destrieux@gmail.com>
2025-02-28 23:23:06 +0100

    conftest files created with fixtures that will be used for tests.

---
+++ tests/test_routers/conftest.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+from typing import Generator
+
+import pytest
+from faker import Faker
+from freezegun import freeze_time
+
+from database.models import Topic
+
+
+@pytest.fixture(scope="class")
+def topic_data_list():
+    fake = Faker()
+    return [
+        {
+            "title": fake.sentence(),
+            "category": fake.word(),
+            "created_by": fake.first_name(),
+        }
+        for _ in range(20)
+    ]
+
+
+@pytest.fixture(scope="class")
+def fake_dates() -> Generator[datetime]:
+    start_date = datetime(2020, 1, 1)
+    return (start_date + timedelta(days=i) for i in range(50))
+
+
+@pytest.fixture(scope="class")
+def bulk_create_topics(db_session, topic_data_list, fake_dates) -> None:
+    for data in topic_data_list:
+        with freeze_time(fake_dates):
+            db_session.add(Topic(**data))
+            db_session.commit()
+
+
+@pytest.fixture
+def create_single_topic(request, db_session) -> Topic:
+    created_by = request.param
+    fake = Faker()
+    topic_obj = Topic(
+        title=fake.sentence(), category=fake.word(), created_by=created_by
+    )
+    db_session.add(topic_obj)
+    db_session.commit()
+    return topic_obj
